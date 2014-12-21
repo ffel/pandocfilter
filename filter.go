53f5dbc374e18e43d822252104128f2bb8333510
Chris van Uffelen <vanuffelen@gmail.com>
2014-12-21 12:08:59 +0100

    cleaned filter.go, separated process

--- filter.go
+++ filter.go
@@ -1,52 +1,47 @@
-// https://gobyexample.com/line-filters
+// based upon (among others) <https://gobyexample.com/line-filters>
+// as an alternative, probably <http://godoc.org/io#PipeReader> could be used
+//
+// cat filter.go | go run filter.go
+//
+// see http://nathanleclaire.com/blog/2014/07/19/demystifying-golangs-io-dot-reader-and-io-dot-writer-interfaces/
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-
-	// Wrapping the unbuffered `os.Stdin` with a buffered
-	// scanner gives us a convenient `Scan` method that
-	// advances the scanner to the next token; which is
-	// the next line in the default scanner.
+	// the default scanner reads line after line
 	scanner := bufio.NewScanner(os.Stdin)
 
 	// the original example does not use stdout explicitly, but it is possible
 	stdout := bufio.NewWriter(os.Stdout)
-
 	defer func() { stdout.Flush() }()
 
-	// as an alternative, probably <http://godoc.org/io#PipeReader> could be used
-
-	// this may be very interesting
-	// http://nathanleclaire.com/blog/2014/07/19/demystifying-golangs-io-dot-reader-and-io-dot-writer-interfaces/
-
-	// try to see intermediate result
-	d, _ := os.Create("dump.json")
-
+	// dump intermediate result
+	d, err := os.Create("dump.json")
 	defer d.Close()
 
-	dump := bufio.NewWriter(d)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 
+	dump := bufio.NewWriter(d)
 	defer func() { dump.Flush() }()
 
+	// use multi as a T that writes to stdout and dump
+	// multi is an io.Writer and cannot be flushed
 	multi := io.MultiWriter(stdout, dump)
 
+	proc := empty{}
+
 	for scanner.Scan() {
-		// `Text` returns the current token, here the next line,
-		// from the input.
-		ucl := strings.ToUpper(scanner.Text())
-
-		// Write out the uppercased line.
-		//fmt.Println(ucl)
-
-		fmt.Fprintln(multi, ucl)
+		fmt.Fprintln(multi, proc.process(scanner.Text()))
 	}
 
 	// Check for errors during `Scan`. End of file is
@@ -57,56 +52,20 @@ func main() {
 	}
 }
 
-// func ExampleWriter() {
-// 	w := bufio.NewWriter(os.Stdout)
-// 	fmt.Fprint(w, "Hello, ")
-// 	fmt.Fprint(w, "world!")
-// 	w.Flush() // Don't forget to flush!
-// 	// Output: Hello, world!
-// }
+type processor interface {
+	process(in string) string
+}
 
-// package main
+type uc struct {
+}
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"io"
-// 	"os"
+func (u uc) process(in string) string {
+	return strings.ToUpper(in)
+}
 
-// 	"github.com/gorilla/websocket"
-// )
+type empty struct {
+}
 
-// func main() {
-
-// }
-
-// // https://gobyexample.com/line-filters
-
-// // http://golang-examples.tumblr.com/post/41863665491/read-a-line-from-stdin
-// func code() {
-// 	bio := bufio.NewReader(os.Stdin)
-// 	line, hasMoreInLine, err := bio.ReadLine()
-// }
-
-// // http://golang.org/src/bufio/example_test.go
-
-// // http://talks.golang.org/2012/chat.slide#39
-
-// type socket struct {
-// 	io.Reader
-// 	io.Writer
-// 	done chan bool
-// }
-
-// var chain = NewChain(2) // 2-word prefixes
-
-// func socketHandler(ws *websocket.Conn) {
-// 	r, w := io.Pipe()
-// 	go func() {
-// 		_, err := io.Copy(io.MultiWriter(w, chain), ws)
-// 		w.CloseWithError(err)
-// 	}()
-// 	s := socket{r, ws, make(chan bool)}
-// 	go match(s)
-// 	<-s.done
-// }
+func (e empty) process(in string) string {
+	return in
+}
