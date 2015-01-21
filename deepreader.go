61fd3c6d767dc6985b871e101465713a67727d28
Chris van Uffelen <vanuffelen@gmail.com>
2015-01-21 20:55:20 +0100

    number test fails

--- deepreader.go
+++ deepreader.go
@@ -5,9 +5,7 @@ import (
 	"strconv"
 )
 
-// GetString retrieves a sting from a deeply nested json
-func GetString(json interface{}, indices ...string) (string, error) {
-	// walk all bu
+func getObject(json interface{}, indices []string) (interface{}, error) {
 	for _, index := range indices /*[:len(slice)-1]*/ {
 		i, err := strconv.Atoi(index)
 		if err == nil {
@@ -15,11 +13,11 @@ func GetString(json interface{}, indices ...string) (string, error) {
 			s, sok := json.(Jslice)
 
 			if !sok {
-				return "", errors.New("GetString error - no slice for index " + index)
+				return "", errors.New("GetObject error - no slice for index " + index)
 			}
 
 			if i < 0 || i >= len(s) {
-				return "", errors.New("GetString error - slice out-of-range index " + index)
+				return "", errors.New("GetObject error - slice out-of-range index " + index)
 			}
 
 			json = s[i]
@@ -29,17 +27,30 @@ func GetString(json interface{}, indices ...string) (string, error) {
 			m, mok := json.(Jmap)
 
 			if !mok {
-				return "", errors.New("GetString error - no map for index " + index)
+				return "", errors.New("GetObject error - no map for index " + index)
 			}
 
 			_, ok := m[index]
 
 			if !ok {
-				return "", errors.New("GetString error reading map - no such key " + index)
+				return "", errors.New("GetObject error reading map - no such key " + index)
 			}
 
 			json = m[index]
 		}
+	}
+
+	return json, nil
+}
+
+// GetString retrieves a sting from a deeply nested json
+func GetString(json interface{}, indices ...string) (string, error) {
+	// walk all bu
+
+	json, err := getObject(json, indices)
+
+	if err != nil {
+		return "", err
 	}
 
 	str, strok := json.(string)
@@ -49,4 +60,21 @@ func GetString(json interface{}, indices ...string) (string, error) {
 	}
 
 	return str, nil
+}
+
+func GetNumber(json interface{}, indices ...string) (float64, error) {
+	json, err := getObject(json, indices)
+
+	if err != nil {
+		return 0, err
+	}
+
+	val, valok := json.(float64)
+
+	if !valok {
+		return 0, errors.New("GetNumber error - no number")
+	}
+
+	return val, nil
+
 }
--- deepreader_test.go
+++ deepreader_test.go
@@ -2,6 +2,37 @@ package pandocfilter
 
 import "testing"
 
+func TestGetNumber(t *testing.T) {
+	data := map[string]interface{}{
+		"t": "Header",
+		"c": []interface{}{
+			1,
+			[]interface{}{
+				"project-opzet",
+				[]interface{}{},
+				[]interface{}{},
+			}, []interface{}{
+				map[string]interface{}{
+					"t": "Str",
+					"c": "Project",
+				}, map[string]interface{}{
+					"t": "Space",
+					"c": []interface{}{},
+				},
+				map[string]interface{}{
+					"t": "Str",
+					"c": "Opzet",
+				}}}}
+
+	val, err := GetNumber(data, "c", "0")
+
+	if err != nil {
+		t.Errorf("unexpected error %v\n", err)
+	} else if val != 0 {
+		t.Errorf("unexpected value %v\n", val)
+	}
+}
+
 func TestGetString(t *testing.T) {
 	data := Jmap{
 		"c": Jslice{
@@ -54,31 +85,31 @@ func TestGetString_errors(t *testing.T) {
 
 	_, err := GetString(data, "0")
 
-	if err.Error() != "GetString error - no slice for index 0" {
+	if err.Error() != "GetObject error - no slice for index 0" {
 		t.Errorf("unexpected error %v", err)
 	}
 
 	_, err = GetString(data, "c", "-1")
 
-	if err.Error() != "GetString error - slice out-of-range index -1" {
+	if err.Error() != "GetObject error - slice out-of-range index -1" {
 		t.Errorf("unexpected error %v", err)
 	}
 
 	_, err = GetString(data, "c", "2")
 
-	if err.Error() != "GetString error - slice out-of-range index 2" {
+	if err.Error() != "GetObject error - slice out-of-range index 2" {
 		t.Errorf("unexpected error %v", err)
 	}
 
 	_, err = GetString(data, "c", "t")
 
-	if err.Error() != "GetString error - no map for index t" {
+	if err.Error() != "GetObject error - no map for index t" {
 		t.Errorf("unexpected error %v", err)
 	}
 
 	_, err = GetString(data, "tc")
 
-	if err.Error() != "GetString error reading map - no such key tc" {
+	if err.Error() != "GetObject error reading map - no such key tc" {
 		t.Errorf("unexpected error %v", err)
 	}
 
